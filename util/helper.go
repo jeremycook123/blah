1a415c3d92f64daeb2008df388abe55fe075056d
Jeremy Cook <jeremycook123@gmail.com>
2019-12-23 15:32:35 +1300

    added Lower func

--- util/helper.go
+++ util/helper.go
@@ -4,7 +4,12 @@ import (
 	"strings"
 ) 
 
-//Hello returns a hello string
+//Upper returns string uppercased
 func Upper(input string) string {
 	return strings.ToUpper(input)
+}
+
+//Lower returns string lowercased
+func Lower(input string) string {
+	return strings.ToLower(input)
 }
